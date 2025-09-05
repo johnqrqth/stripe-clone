b373167509b4b5d93bee6ffc12071e50b01e4b97
John Thiong'o <john@djinndigital.com>
2025-09-05 16:08:39 +0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import NavHeader from "@/components/NavHeader";
+import Footer from "@/components/Footer";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Stripe Sque Clone",
+  description: "A clone of the stripe landing page UI",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <NavHeader />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+     <main className="flex flex-col items-center justify-center gap-8 h-[100vh]">
+      <div className="text-2xl font-bold text-black"> A stripe clone </div>
+     </main>
+    </div>
+  );
+}
